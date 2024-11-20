ee5880e091bf656fa61ac52ef5e716881157c68d
azu-cyber <avikalizhimomi@gmail.com>
2024-11-20 17:50:41 +0530

    OddEven

--- main.py
+++ main.py
@@ -12,3 +12,14 @@ num1=10
 num2=4
 print("\nnum1 < 1=", num1<<1)
 print("\nnum < 1=", num2<<1)
+
+def isEvenOdd( n) :
+    if (n ^ 1 ==n + 1) :
+        return True;
+    else :
+        return False;
+number = int(input("Enter your number : "))
+if isEvenOdd (number):
+    print(number," is Even")
+else:
+    print(number," is Odd")
